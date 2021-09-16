92b500df4e132714491455655475a48cabe44cfd
cherish <cherishzhang@sohu-inc.com>
2021-09-16 20:58:34 +0800

    first upload

--- quantumultx-demo.js
+++ quantumultx-demo.js
@@ -1,10 +1,12 @@
-console.log(2311222);
+console.log(1);
+console.log($response.statusCode);
+console.log(2);
 var body = $response.body;
 // var obj = JSON.parse(body);
 
 // obj['success'] = true;
 // body = JSON.stringify(obj);
-console.log(231);
+console.log(3);
 console.log(body);
 
 $done(body);
